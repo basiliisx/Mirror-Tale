837cf9f3aef539eb34ba74bfc02236fce67892f2
Zygmut <Zygmut@users.noreply.github.com>
2019-04-09 15:48:56 +0200

    Ver 01
    - Fixed some bugs
    - Added NPC Troll

--- Mirror-Tale-RPG/src/mirror/tale/rpg/NPC.java
+++ Mirror-Tale-RPG/src/mirror/tale/rpg/NPC.java
@@ -1,19 +1,20 @@
-package mirror.tale.rpg;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package mirror.tale.rpg;
 
 /**
  *
  * @author Zygmut
  */
 public class NPC {
-    String name;
-    String id = "NPC";
-    String desc;
-    Integer hp;
-    Integer accuracy;
+    public NPC (){
+        String name;
+        String desc;
+        String id= "NPC";
+        int hp;
+        int accuracy;
+    }
 }
--- Mirror-Tale-RPG/src/mirror/tale/rpg/Troll.java
+++ Mirror-Tale-RPG/src/mirror/tale/rpg/Troll.java
@@ -1,16 +1,19 @@
-package mirror.tale.rpg;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package mirror.tale.rpg;
 
 /**
  *
  * @author Zygmut
  */
 public class Troll extends NPC {
-    
+
+    String name = "A troll";
+    String desc = "A troll standing menacing";
+    String id = "Troll";
+    int hp = 10;
+    int accuracy = 25;
 }
